3554a2c1408dd6d9519e3ab6aa522fbb99f93490
nikolnikon <nikolnikon@gmail.com>
2018-07-24 18:31:04 +0300

    Описал модели, прикрутил configurations 1. Описал базвые поля моделей (без связей many to many) 2. Поправил manage.py и wsgi.py для работы с configurations

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+# todo Добавить связь работодателей с курсами
+# todo Добавить связь преподавателей с курсами
+
+
+# todo Подумать насчет запихивания модулей и уроков в json
+class Course(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    description = models.TextField()
+    price = models.PositiveSmallIntegerField()
+    duration = models.PositiveSmallIntegerField()
+    start_date = models.DateTimeField()
+    load = models.TextField()
+    logo = models.ImageField(upload_to='images/courses/')
+    
+
+class Module(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='modules')
+
+
+# todo Подумать насчет вынесения homework в отдельную модель
+class Lesson(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(null=True)
+    date = models.DateTimeField()
+    homework = models.TextField(null=True)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='lessons')
+
+
+class Employer(models.Model):
+    name = models.CharField(max_length=256)
+    about = models.TextField()
+    logo = models.ImageField(upload_to='images/employers/')
