1680065e61deab824c80f452f30e3a94d8e456d4
nikolnikon <nikolnikon@gmail.com>
2018-07-30 18:43:16 +0300

    Сделал в первом приближении страницу со списком курсов 1. Адаптировал шаблон от магазина 2. Добавил favicon

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from courses.views import CourseListView, CourseDetailView
 
 
+app_name = 'courses'
+
 urlpatterns = [
-    path('', CourseListView.as_view(), name='detail'),
+    path('', CourseListView.as_view(), name='list'),
     path('<int:pk>/', CourseDetailView.as_view(), name='detail'),
 ]
